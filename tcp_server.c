2aac1d7bc71a1ce62c923a016165b156cb44cda5
zurnachshon <zurnachshon@gmail.com>
2017-05-08 21:44:42 -0700

    Revert "checks for Protocol 4, 5, and 6"
    This reverts commit b51ae9a63971e1d8552f012fd0ee8f859932a690.

--- tcp_server.c
+++ tcp_server.c
@@ -34,12 +34,12 @@ void main(int argc,char *argv[])
   server_addr.sin_addr.s_addr = INADDR_ANY;
 
   /*********************binding ipaddr and portno***********************/
-  if(bind(sds,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0)
-  {
+  if(bind(sds,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0) 
+  { 
     perror("Failed to bind\n");
     exit(1);
   }
-
+  
   /**********************Server in Listening mode***********************/
   listen(sds,5);
 
@@ -49,52 +49,37 @@ void main(int argc,char *argv[])
   addrlen = sizeof(client_addr);
   newsd = accept(sds,(struct sockaddr *)&client_addr,&addrlen);
   printf("newsd = %d\n",newsd);
-  if(newsd < 0)
+  if(newsd < 0) 
   {
     perror("Failed to accept from client\n");
     exit(1);
   }
-
+  
   /***********getrusage************************
   //for(i=0;i<10000;i++) sum+=i;
   pUsage  = getrusage(RUSAGE_SELF,&usage);
   sprintf(pUsageChr,"%d",pUsage);
   printf("process usage = %d %s\n",pUsage,pUsageChr);/
 
-  /******************Reading data from client***************************/
+  /******************Reading data from client***************************/ 
   while(1){
       bzero(buffer,256);
-      char sub1[11] = {0};
-      bzero(sub1, 11);
-
+  
       n = read(newsd,buffer,255);
+      printf("message = %s\n",buffer);
       if(n<0)
         printf("Error in reading data\n");
+    
 
-      memcpy( sub1, buffer, 10);
-      if(strcmp(sub1, "Protocol 4") == 0){
-        printf("You have entered %s\n", buffer);
-      }
-      else if(strcmp(sub1, "Protocol 5") == 0){
-        printf("You have entered %s\n", buffer);
-      }
-      else if(strcmp(sub1, "Protocol 6") == 0){
-        printf("You have entered %s\n", buffer);
-      }
-      else{
-        printf("message = %s\n",buffer);
-        // int help = strcmp(buffer, "Protocol 5");
-        // printf("The result is: %d\n", help);
-      }
-
-  /******************Writing data to client*****************************/
+  /******************Writing data to client*****************************/ 
   n = write(newsd,buffer,23);
   //n = write(newsd,pUsageChr,10);
   if(n<0)
-    printf("Error in writing data\n");
+    printf("Error in writing data\n");  
 }
-
+  
 }
   /******************Close the Socket***********************************/
   close(sds);
 }
+
